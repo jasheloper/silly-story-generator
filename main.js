f37671347848d7887aa22b34a62f63ba5f8b3851
jasheloper <48870048+jasheloper@users.noreply.github.com>
2023-09-07 19:21:48 -0700

    Initial variables and functions

---
+++ main.js
@@ -0,0 +1,25 @@
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random()*array.length);
+  return array[random];
+}
+
+let storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+
+let insertX = [
+    "Willy the Goblin",
+    "Big Daddy",
+    "Father Christmas"];
+
+let insertY = [
+    "the soup kitchen",
+    "Disneyland",
+    " the White House"];
+
+let insertZ = [
+    "spontaneously combusted",
+    "melted into a puddle on the sidewalk",
+    "turned into a slug and crawled away"];
