772796ba42b3c7fc184ee21cb92baf55065ade30
jasheloper <48870048+jasheloper@users.noreply.github.com>
2023-09-07 19:54:13 -0700

    result function wip

--- main.js
+++ main.js
@@ -13,8 +13,28 @@ const story = document.querySelector('.story');
 
 
 
-
 // text strings that will act as input into our program.
+
+
+/*
+
+Next we want to replace the three placeholders in the newStory string — 
+
+:insertx:, :inserty:, and :insertz: — with the strings stored in xItem, yItem, and zItem. 
+
+There are two possible string methods that will help you here — 
+
+in each case, make the call to the method equal to newStory, 
+
+so each time it is called, newStory is made equal to itself, 
+
+but with substitutions made. 
+
+So each time the button is pressed, these placeholders are each replaced with a random silly string. 
+
+As a further hint, depending on the method you choose, you might need to make one of the calls twice.
+
+*/
 
 let storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
 
@@ -32,6 +52,38 @@ let insertZ = [
     "spontaneously combusted",
     "melted into a puddle on the sidewalk",
     "turned into a slug and crawled away"];
+
+
+
+
+
+/*
+Create a new variable called newStory, and set its value to equal storyText. 
+
+This is needed so we can create a new random story each time the button is pressed and the function is run. 
+
+If we made changes directly to storyText, we'd only be able to generate a new story once.
+*/ 
+
+let newStory = storyText;
+
+
+
+/*
+Create three new variables called xItem, yItem, and zItem, 
+
+and make them equal to the result of calling randomValueFromArray() on your three arrays 
+
+(the result in each case will be a random item out of each array it is called on). 
+
+For example you can call the function and get it to return one random string out of insertX by writing randomValueFromArray(insertX).
+*/
+
+let xItem = randomValueFromArray(insertX);
+let yItem = randomValueFromArray(insertY);
+let zItem = randomValueFromArray(insertZ);
+
+
 
 
 
