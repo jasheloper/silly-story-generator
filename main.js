13590c1809225893da28c9dae8d86131db2e5df7
jasheloper <48870048+jasheloper@users.noreply.github.com>
2023-09-07 19:23:32 -0700

    adds event handler and incomplete function

--- main.js
+++ main.js
@@ -23,3 +23,24 @@ let insertZ = [
     "spontaneously combusted",
     "melted into a puddle on the sidewalk",
     "turned into a slug and crawled away"];
+
+
+
+randomize.addEventListener('click', result);
+
+function result() {
+
+    if(customName.value !== '') {
+    const name = customName.value;
+
+    }
+
+    if(document.getElementById("uk").checked) {
+    const weight = Math.round(300);
+    const temperature =  Math.round(94);
+
+    }
+
+    story.textContent = ;
+    story.style.visibility = 'visible';
+}
