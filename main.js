7a6a23a8be85e979f0a543ae491f5cf1e6aa8c71
jasheloper <48870048+jasheloper@users.noreply.github.com>
2023-09-08 20:03:22 -0700

    first if block

--- main.js
+++ main.js
@@ -60,7 +60,7 @@ function result() {
 
   if(customName.value !== '') {
     const name = customName.value;
-
+    newStory.replace("Bob", name);
   }
 
   if(document.getElementById("uk").checked) {
