46badf63e5b0d51858d3e6f5999d748cfc366372
jasheloper <48870048+jasheloper@users.noreply.github.com>
2023-09-13 18:48:04 -0700

    got it working

--- main.js
+++ main.js
@@ -36,33 +36,23 @@ function result() {
   let yItem = randomValueFromArray(insertY);
   let zItem = randomValueFromArray(insertZ);
 
-  newStory.replaceAll(":insertx:", xItem).replace(":inserty:", yItem).replace(":insertz:", zItem);
+  newStory = newStory.replaceAll(":insertx:", xItem).replace(":inserty:", yItem).replace(":insertz:", zItem);
 
 
   if(customName.value !== '') {
     const name = customName.value;
-    newStory.replace("Bob", name);
-    console.log(
-      name
-    )
+    newStory = newStory.replace("Bob", name);
+
   }
 
   if(document.getElementById("uk").checked) {
     const weight = `${Math.round((300 / 14))} stone`;
     const temperature =  `${Math.round((94 - 32) * (5 / 9))} centigrade`;
-    newStory.replace("300 pounds", weight).replace("94 fahrenheit", temperature);
-    console.log(`
-      ${weight}
-      ${temperature}
-    `)
+    newStory = newStory.replace("300 pounds", weight).replace("94 fahrenheit", temperature);
   }
 
   story.textContent = newStory;
   story.style.visibility = 'visible';
-
-  console.log(
-    newStory.replaceAll(":insertx:", xItem).replace(":inserty:", yItem).replace(":insertz:", zItem)
-  )
 }
 
 
